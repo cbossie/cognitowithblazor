be9088c2a60884f5b9d5b5262fd0b791e668df2c
cbossie <cbossie@gmail.com>
2022-01-05 22:02:08 -0500

    COnfig

--- webapi/Model/CognitoSettings.cs
+++ webapi/Model/CognitoSettings.cs
@@ -5,5 +5,7 @@ public class CognitoSettings
     public string? Authority { get; set; }
     public string? ClientId { get; set; }
 
-    public string? JwtKeysetUrl { get; set; }
+    public string? UserPoolId { get; set; }
+
+    public string? PoolDomain { get; set; }
 }
--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Net;
+using Amazon.Extensions.Configuration.SystemsManager;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using System.Text.Json;
 using webapi.Model;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var awsOptions = builder.Configuration.GetAWSOptions();
+builder.Configuration.AddSystemsManager(configSource => {
+    configSource.Path = "/cognitoapi/";
+    configSource.AwsOptions = awsOptions;
+});
+
+
 
 // Add services to the container.
 
@@ -21,8 +32,10 @@ builder.Services.AddCors(policy =>
         .AllowAnyMethod());
 });
 
+
 CognitoSettings cognito = new();
-builder.Configuration.Bind("Cognito", cognito);
+builder.Configuration.Bind(cognito);
+
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -41,11 +54,8 @@ builder.Services.AddAuthentication(options =>
 
     });
 
-
-
-
-
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
